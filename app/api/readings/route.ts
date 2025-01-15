6cebc6f745fe7940b41249c81909572c555ebd76
daw-dev <dawdev04@gmail.com>
2025-01-15 17:23:50 +0100

    unused stuff

--- app/api/readings/route.ts
+++ app/api/readings/route.ts
@@ -1,7 +1,7 @@
 import { TemperatureHumidity } from "@/app/utils/database";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const database = TemperatureHumidity();
   const collection = database.collection("readings");
   const readings = await collection.find().toArray();
--- app/utils/database.ts
+++ app/utils/database.ts
@@ -1,4 +1,4 @@
-import { Db, MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 
 const MONGO_URI = process.env.MONGO_URI;
 const client = await new MongoClient(MONGO_URI ?? "").connect();
